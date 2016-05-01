a8d89e8b24ba094ae006029079e779ff0c0b40df
pgbcore <pabgb95@gmail.com>
2016-05-01 13:08:53 +0200

    Tema 2: Primera págin y favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
+});
+
+/* GET author page. */
+router.get('/author', function(req, res, next) {
+  res.render('author', { title: 'Quiz' });
 });
 
 module.exports = router;
