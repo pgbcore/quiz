b4230364404c37d974dc73c2f7b2fc0c4c957d9b
pgbcore <pabgb95@gmail.com>
2016-05-21 01:37:20 +0200

    Tema 10:  Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,8 @@ router.get('/author', function(req, res, next) {
   res.render('author');
 });
 
-router.get('/question', quizController.question);
-router.get('/check', quizController.check);
+router.get('/quizzes', quizController.index);
+router.get('/quizzes/:quizId(//d+)', quizController.show);
+router.get('/quizzes/:quizId(//d+)/check', quizController.check);
 
 module.exports = router;
